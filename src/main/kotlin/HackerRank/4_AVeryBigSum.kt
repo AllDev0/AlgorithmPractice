d2011b509e01c2648e6c409d7e937cbf6880a20c
AllDev0 <helloworlddeveloper0@gmail.com>
2023-10-08 13:13:21 +0300

    HackerRank - 4_AVeryBigSum

---
+++ src/main/kotlin/HackerRank/4_AVeryBigSum.kt
@@ -0,0 +1,7 @@
+package HackerRank
+
+fun main() {
+    println(aVeryBigSum(arrayOf(1000000001, 1000000002, 1000000003, 1000000004, 1000000005)))
+}
+
+fun aVeryBigSum(ar: Array<Long>): Long = ar.sum()
