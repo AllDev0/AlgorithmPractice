78ce16821e7ba483d891c78b15e3aee72d1da4b8
AllDev0 <helloworlddeveloper0@gmail.com>
2023-10-08 12:52:52 +0300

    HackerRank - 1_SolveMeFirst

---
+++ src/main/kotlin/HackerRank/1_SolveMeFirst.kt
@@ -0,0 +1,9 @@
+package HackerRank
+
+// https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+fun main() {
+    println(solveMeFirst(9,8))
+}
+
+fun solveMeFirst(a: Int, b: Int): Int = a + b
